cf222551cf4f5f69fa753e793b7951070a614e8d
Emmanuel oshakpemeh <149076550+imacul@users.noreply.github.com>
2026-09-19 20:37:28 +0100

    Fix Vercel asset base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-export default defineConfig({ plugins:[react()], base:"/skycanvas/" });
+
+export default defineConfig({
+  plugins: [react()],
+  base: "/",
+});
